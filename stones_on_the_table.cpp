1df6bf4087338fdd24b9de045043e13de6a63082
rudra-404 <rudrakaiser840@gmail.com>
2024-05-13 19:53:50 +0600

    Stones on the Table

---
+++ stones_on_the_table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,count=0;
+	cin>>n;
+	string arr;
+	cin>>arr;
+	for(int i=1; i<n; i++){
+		if(arr[i-1]==arr[i]) count++;
+	}cout<<count;
+	return 0;
+}
