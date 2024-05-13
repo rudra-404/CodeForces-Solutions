70b073e48bd18469610cdd80fbc9d55a920fd7a9
rudra-404 <rudrakaiser840@gmail.com>
2024-05-13 16:15:25 +0600

    boy or girl solved in cpp

---
+++ boy_or_girl.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string username;
+	cin>>username;
+	set<char> userset;
+	for(int i=0; i<username.size(); i++){
+		userset.insert(username[i]);
+	}
+	if(userset.size()%2==0){
+		cout<<"CHAT WITH HER!";
+	}else{
+		cout<<"IGNORE HIM!";
+	}
+	return 0;
+}
