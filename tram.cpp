3d7f052174feba37da14fa71d9d58858ddf628e8
rudra-404 <rudrakaiser840@gmail.com>
2024-05-17 12:34:09 +0600

    116A

---
+++ tram.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int capacity=0;
+	int stillnow=0;
+	for(int i=0;i<n;i++){
+		int passanger_out, passanger_in;
+		cin>>passanger_out>>passanger_in;
+		stillnow-=passanger_out;
+		stillnow+=passanger_in;
+		capacity=max(capacity,stillnow);
+	}
+	cout<<capacity;
+	return 0;
+}
