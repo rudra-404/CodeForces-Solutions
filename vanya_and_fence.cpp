428e278342a8a5bbfa824e20fe892af60d9070c2
rudra-404 <rudrakaiser840@gmail.com>
2024-05-17 15:51:56 +0600

    pro

---
+++ vanya_and_fence.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,h,w=0;
+	cin>>n>>h;
+	for(int i=0; i<n; i++){
+		int a;
+		cin>>a;
+		if(a>h) w+=2;
+		else w+=1;
+	}
+	cout<<w;
+	return 0;
+}
+
+//677A
