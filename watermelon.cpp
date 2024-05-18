f0d393f3712300dff1934c28316cac7691798c5d
rudra-404 <rudrakaiser840@gmail.com>
2024-05-18 23:16:16 +0600

    4A

--- watermelon.cpp
+++ watermelon.cpp
@@ -3,10 +3,7 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	if(n%2==0 && n!=2){
-		cout<<"YES";
-	}else{
-		cout<<"NO";
-	}
+	if(n%2==0 && n!=2) cout<<"YES";
+	else cout<<"NO";
 	return 0;
 }
