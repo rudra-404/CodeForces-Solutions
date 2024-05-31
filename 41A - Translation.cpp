b766522edc1e3d2be184fd260260552b2fe922eb
rudra-404 <rudrakaiser840@gmail.com>
2024-05-31 12:05:46 +0600

    41A - Translation

--- 41A - Translation.cpp
+++ 41A - Translation.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	string a,b;
-	cin>>a>>b;
-	int count=0, j=a.size()-1;
-	for(int i=0; i<a.size(); i++){
-		if(a[i]==b[j]){
+	string s, t;
+	cin>>s>>t;
+	int count=0, j=s.size()-1;
+	for(int i=0; i<s.size(); i++){
+		if(s[i]==t[j]){
 			count++;
 		}
 		j--;
 	}
-	if(count==a.size()) cout<<"YES";
+	if(count==s.size()) cout<<"YES";
 	else cout<<"NO";
 	return 0;
 }
