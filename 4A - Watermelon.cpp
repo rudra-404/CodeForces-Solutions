006c7c4a7981272f1644ddf53bd65bf4d54d9858
rudra-404 <rudrakaiser840@gmail.com>
2024-05-31 12:01:31 +0600

    4A - Watermelon

--- 4A - Watermelon.cpp
+++ 4A - Watermelon.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int n;
-	cin>>n;
-	if(n%2==0 && n!=2) cout<<"YES";
+	int weight;
+	cin>>weight;
+	if(weight%2==0 && weight!=2) cout<<"YES";
 	else cout<<"NO";
 	return 0;
 }
