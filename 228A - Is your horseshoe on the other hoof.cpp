9f28376abf548510bc8b4d531066d4facd11b948
rudra-404 <rudrakaiser840@gmail.com>
2024-08-12 10:30:19 +0600

    228A - Is your horseshoe on the other hoof?

---
+++ 228A - Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[4], count=0;
+    cin>>a[0]>>a[1]>>a[2]>>a[3];
+    sort(a, a+4);
+    for(int i=1; i<4; i++) if(a[i]==a[i-1]) count++;
+    cout<<count;
+    return 0;
+}
