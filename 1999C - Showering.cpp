80f49371bb0ddfd1d07d24670680f47d41c1982f
rudra-404 <rudrakaiser840@gmail.com>
2024-08-15 18:42:12 +0600

    1999C - Showering

---
+++ 1999C - Showering.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); // Fast I/O = this line is for time complexity
+    int t, n, s, m, previous, start, end;
+    bool bath;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> s >> m;
+        previous = 0;
+        bath = false;
+        for(int i=0; i<n; i++)
+        {
+            cin >> start >> end;
+            if(start-previous >= s) bath = true;
+            previous = end;
+        }
+        if(m-end >= s) bath = true;
+        cout << (bath ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
