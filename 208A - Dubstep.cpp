5112cac2f3c31f278eed3620f8064c541efd264d
rudra-404 <rudrakaiser840@gmail.com>
2024-08-16 16:56:37 +0600

    208A - Dubstep

---
+++ 208A - Dubstep.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    for(int i=0; i<str.size(); i++)
+    {
+        if(str[i]=='W' && str[i+1]=='U' && str[i+2]=='B')
+        {
+            i+=2;
+            cout << " ";
+        }
+        else cout << str[i];
+    }
+    return 0;
+}
