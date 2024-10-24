b4e5b2daa5b1df08edb7806fd1b347f2d868c941
rudra-404 <rudrakaiser840@gmail.com>
2024-10-24 16:49:40 +0600

    214A - System of Equations

--- 214A - System of Equations.cpp
+++ 214A - System of Equations.cpp
@@ -1,18 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(false);
     int n, m, count = 0;
     cin >> n >> m;
-    for(int a=0; a<=m; a++)
-    {
-        for(int b=0; b<=n; b++)
-        {
-            if( ((a*a)+b == n) && (a+(b*b) == m) )
-            {
-                count++;
-            }
+    for(int a=0; a<=m; a++){
+        for(int b=0; b<=n; b++){
+            if( ((a*a)+b == n) && (a+(b*b) == m) ) count++;
         }
     }
     cout << count;
