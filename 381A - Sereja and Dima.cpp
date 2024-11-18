a9ffb9c85905f7008753465f97353f9fda6e6ed1
rudra-404 <rudrakaiser840@gmail.com>
2024-11-19 00:04:47 +0600

    C++

--- 381A - Sereja and Dima.cpp
+++ 381A - Sereja and Dima.cpp
@@ -3,32 +3,18 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> cards(n);
-    for (int i=0; i<n; i++) cin >> cards[i];
-    int sreja = 0, dima = 0;
+    int arr[n];
+    for (int i=0; i<n; i++) cin >> arr[i];
+    int left = 0, right = n-1, sreja = 0, dima = 0;
     bool isSreja = true;
-    while (cards.size() > 0) {
+    while (left <= right) {
         if (isSreja) {
-            int m = max(cards[0],cards[cards.size()-1]);
-            sreja += m;
-            if (m == cards[0]) {
-                auto it = cards.begin();
-                cards.erase(it);
-            } else {
-                auto it = cards.end()-1;
-                cards.erase(it);
-            }
+            if (arr[left] > arr[right]) sreja += arr[left++];
+            else sreja += arr[right--];
             isSreja = false;
         } else {
-            int m = max(cards[0],cards[cards.size()-1]);
-            dima += m;
-            if (m == cards[0]) {
-                auto it = cards.begin();
-                cards.erase(it);
-            } else {
-                auto it = cards.end()-1;
-                cards.erase(it);
-            }
+            if (arr[left] > arr[right]) dima += arr[left++];
+            else dima += arr[right--];
             isSreja = true;
         }
     }
