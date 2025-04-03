d18e6d566d6f2a443ca4a18ec17232d691169384
rudra-404 <rudrakaiser840@gmail.com>
2025-04-04 00:05:52 +0600

    C++

---
+++ SOLVE/1343A - Candies.cpp
@@ -0,0 +1,35 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll n;
+    cin >> n;
+    for (int k = 2; k <= 35; k++) {
+        int x = n / ( pow(2, k) - 1 );
+        if (n % x) continue;
+        cout << x << endl;
+        return;
+    }
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
