da089a6fcf8743f580649e6f003779dd1512ad93
rudra-404 <rudrakaiser840@gmail.com>
2025-04-09 23:54:33 +0600

    C++

---
+++ SOLVE/709A - Juicer.cpp
@@ -0,0 +1,39 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n, b, d;
+    cin >> n >> b >> d;
+    int waste = 0, count = 0;
+    for (int i = 0; i < n; i++) {
+        int orange;
+        cin >> orange;
+        if (orange > b) continue;
+        waste += orange;
+        if (waste > d) {
+            count++;
+            waste = 0;
+        }
+    }
+    cout << count << endl;
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
