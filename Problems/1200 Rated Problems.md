43486752004cb77bcf9c51d8fc47c798e084351c
rudra-404 <rudrakaiser840@gmail.com>
2025-04-10 13:01:13 +0600

    C++

--- Problems/1200 Rated Problems.md
+++ Problems/1200 Rated Problems.md
@@ -1,6 +1,6 @@
 # Rating 1200*
 | Problems | Problems | Problems |
 | :- | :- | :- |
-| [A. Odd Selection](https://codeforces.com/problemset/problem/1363/A) | [A. Cards with Numbers](https://codeforces.com/problemset/problem/254/A) | []() |
+| [A. Odd Selection](https://codeforces.com/problemset/problem/1363/A) | [A. Cards with Numbers](https://codeforces.com/problemset/problem/254/A) | [B. Kuriyama Mirai's Stones](https://codeforces.com/contest/433/problem/B) |
 | []() | []() | []() |
 | []() | []() | []() |
---
+++ SOLVE/433B - Kuriyama Mirai's Stones.cpp
@@ -0,0 +1,48 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<long long> v(n + 1), pv(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+        pv[i] = pv[i - 1] + v[i];
+    }
+    vector<long long> u = v, pu(n + 1);
+    sort(u.begin() + 1, u.end());
+    for (int i = 1; i <= n; i++) {
+        pu[i] = pu[i - 1] + u[i];
+    }
+    int m;
+    cin >> m;
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            cout << pv[c] - pv[b - 1] << endl;
+        } else {
+            cout << pu[c] - pu[b - 1] << endl;
+        }
+    }
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
