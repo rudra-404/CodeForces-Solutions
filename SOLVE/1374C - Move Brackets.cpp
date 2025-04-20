1f75a00831eebe95e069ec88513dc29705f1697e
rudra-404 <rudrakaiser840@gmail.com>
2025-04-20 14:08:53 +0600

    C++

---
+++ SOLVE/1374C - Move Brackets.cpp
@@ -0,0 +1,40 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int opening_bracket = 0, closing_bracket = 0, move = 0;
+    for (char ch : s) {
+        if (ch == '(') opening_bracket++;
+        else closing_bracket++;
+        if (closing_bracket > opening_bracket) {
+            move++;
+            closing_bracket--;
+        }
+    }
+    cout << move << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
