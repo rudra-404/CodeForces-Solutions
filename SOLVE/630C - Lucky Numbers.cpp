728ff72e8e31ff62364ef360e96e75b87aa30ab8
rudra-404 <rudrakaiser840@gmail.com>
2025-04-20 14:08:29 +0600

    C++

---
+++ SOLVE/630C - Lucky Numbers.cpp
@@ -0,0 +1,29 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll n;
+    cin >> n;
+    cout << (1LL << (n + 1)) - 2 << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    solve();
+    return 0;
+}
