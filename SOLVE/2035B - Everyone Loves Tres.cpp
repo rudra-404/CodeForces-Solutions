9a39806722df81c79af55a144243b4928b31714f
rudra-404 <rudrakaiser840@gmail.com>
2025-04-20 14:09:35 +0600

    C++

---
+++ SOLVE/2035B - Everyone Loves Tres.cpp
@@ -0,0 +1,42 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n == 1 || n == 3) {
+        cout << "-1" << endl;
+    } else if (n & 1) {
+        for (int i = 1; i <= n - 5; i++) {
+            cout << '3';
+        }
+        cout << "36366" << endl;
+    } else {
+        for (int i = 1; i <= n - 2; i++) {
+            cout << '3';
+        }
+        cout << "66" << endl;
+    }
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
