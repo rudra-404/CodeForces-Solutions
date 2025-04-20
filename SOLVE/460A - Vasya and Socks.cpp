53ebe8c8c3b9ae981c68519c0a0bc7cc24b74d8a
rudra-404 <rudrakaiser840@gmail.com>
2025-04-20 14:08:14 +0600

    C++

---
+++ SOLVE/460A - Vasya and Socks.cpp
@@ -0,0 +1,36 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    int day = 0, socks = n;
+    while (socks > 0) {
+        day++;
+        socks--;
+        if (day % m == 0) {
+            socks++;
+        }
+    }
+    cout << day << endl;
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
