8325e7cdabc4ff351459b4abf4a198d3cad99035
rudra-404 <rudrakaiser840@gmail.com>
2025-04-23 18:13:19 +0600

    C++

--- SOLVE/1560A - Dislike of Threes.cpp
+++ SOLVE/1560A - Dislike of Threes.cpp
@@ -1,19 +1,37 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int test;
-    cin >> test;
-    while (test--) {
-        int k;
-        cin >> k;
-        for (int i=1; ; i++) {
-            if (i%3 == 0 || i%10 == 3) continue;
-            k--;
-            if (k == 0) {
-                cout << i << "\n";
-                break;
-            }
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; ; i++) {
+        if (i % 3 == 0 || i % 10 == 3) continue;
+        n--;
+        if (n == 0) {
+            cout << i << endl;
+            break;
         }
     }
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
     return 0;
 }
