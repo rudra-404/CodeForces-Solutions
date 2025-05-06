8e1fc1a1fca2c29f2c84a679e8a844311bbc5573
rudra-404 <rudrakaiser840@gmail.com>
2025-05-06 13:13:05 +0600

    C++

---
+++ SOLVE/1837B - Comparison String.cpp
@@ -0,0 +1,41 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int maximum = 1, longest = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) {
+            longest++;
+        } else {
+            maximum = max(longest, maximum);
+            longest = 1;
+        }
+    }
+    maximum = max(longest, maximum) + 1;
+    cout << maximum << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
