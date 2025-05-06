fd97bcf7ec2fd6c319e90fecc96a6c41952c5d16
rudra-404 <rudrakaiser840@gmail.com>
2025-05-06 13:12:10 +0600

    C++

---
+++ SOLVE/1440B - Sum of Medians.cpp
@@ -0,0 +1,38 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    ll arr[n * k];
+    for (auto &i : arr) {
+        cin >> i;
+    }
+    ll sum = 0;
+    for (int i = (n * k) - (n / 2) - 1; k--; i = i - (n / 2) - 1) {
+        sum += arr[i];
+    }
+    cout << sum << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
