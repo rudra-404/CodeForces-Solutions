3aa69eb7bdf4d1c4db013c9f27579b0698baca7c
rudra-404 <rudrakaiser840@gmail.com>
2025-05-06 13:13:16 +0600

    C++

---
+++ SOLVE/1875A - Jellyfish and Undertale.cpp
@@ -0,0 +1,41 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll a, b, n;
+    cin >> a >> b >> n;
+    ll arr[n];
+    for (auto &i:arr) {
+        cin >> i;
+    }
+    ll timer = b - 1;
+    b = 1;
+    for (ll i = 0; i < n; i++) {
+        b = min(b + arr[i], a);
+        timer += (b - 1);
+        b = 1;
+    }
+    cout << timer + b << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
