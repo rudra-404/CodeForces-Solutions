46ab3134a91eb88b58c958a685fc29bf60a9460a
rudra-404 <rudrakaiser840@gmail.com>
2025-05-08 14:24:47 +0600

    C++

---
+++ SOLVE/742A - Arpa’s hard exam and Mehrdad’s naive cheat.cpp
@@ -0,0 +1,39 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll unsigned long long int
+#define endl '\n'
+
+void solve() {
+    ll power;
+    cin >> power;
+    ll base = 8;
+    ll result = 1;
+    while (power) {
+        if (power & 1) {
+            result = (result * base) % 10;
+            power = power - 1;
+        } else {
+            base = (base * base) % 10;
+            power = power / 2;
+        }
+    }
+    cout << result << endl;
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
