02b164ae0c66b0a4ca9bc025b4b3e99f76e35676
rudra-404 <rudrakaiser840@gmail.com>
2025-05-09 17:00:59 +0600

    C++

---
+++ SOLVE/1326B - Maximums.cpp
@@ -0,0 +1,38 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    ll b[n];
+    for (auto &i : b) {
+        cin >> i;
+    }
+    ll a[n];
+    ll x = 0;
+    for (int i = 0; i < n; i++) {
+        a[i] = b[i] + x;
+        cout << a[i] << ' ';
+        x = max (x, a[i]);
+    }
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
