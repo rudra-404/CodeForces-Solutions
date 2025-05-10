fba53572bb72d5034f7c17c6dd32656d273c60f0
rudra-404 <rudrakaiser840@gmail.com>
2025-05-10 23:31:37 +0600

    C++

---
+++ SOLVE/1543A - Exciting Bets.cpp
@@ -0,0 +1,41 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    if (a == b) {
+        cout << "0 0" << endl;
+        return;
+    }
+    ll difference = abs(a - b);
+    ll x = a % difference;
+    ll y = b % difference;
+    if (x != y) {
+        cout << "0 0" << endl;
+        return;
+    }
+    cout << difference << ' ' << min(x, abs(difference - x)) << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
