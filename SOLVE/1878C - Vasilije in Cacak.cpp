ca7db0414e9b467abacc57a949a1fc4c42fd4107
rudra-404 <rudrakaiser840@gmail.com>
2025-05-15 12:58:22 +0600

    C++

---
+++ SOLVE/1878C - Vasilije in Cacak.cpp
@@ -0,0 +1,33 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll n, k, x;
+    cin >> n >> k >> x;
+    ll min_sum = (k * (k + 1)) / 2;
+    ll max_sum = (k * ((2 * n) + 1 - k)) / 2;
+    if (x >= min_sum && x <= max_sum) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
