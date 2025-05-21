512ad5d0eb7cc180452005f9ef749e829f2e75a1
rudra-404 <rudrakaiser840@gmail.com>
2025-05-21 19:29:45 +0600

    C++

---
+++ SOLVE/1607B - Odd Grasshopper.cpp
@@ -0,0 +1,41 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll x, n;
+    cin >> x >> n;
+    ll jump = n % 4;
+    ll total = (n / 4) * 4;
+    total++;
+    while (jump--) {
+        if (x & 1) {
+            x += total;
+        } else {
+            x -= total;
+        }
+        total++;
+    }
+    cout << x << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
