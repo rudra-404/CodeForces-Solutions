06085bbeca606b7ee4d6b9217dcb50d486e79998
rudra-404 <rudrakaiser840@gmail.com>
2025-05-28 23:04:15 +0600

    C++

---
+++ SOLVE/1650B - DIV + MOD.cpp
@@ -0,0 +1,34 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    ll l, r, a;
+    cin >> l >> r >> a;
+    ll val_1 = (r / a) + (r % a);
+    ll x = (r / a) * a - 1;
+    ll val_2 = -1;
+    if (x >= l) val_2 = (x / a) + (x % a);
+    cout << max(val_1, val_2) << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
