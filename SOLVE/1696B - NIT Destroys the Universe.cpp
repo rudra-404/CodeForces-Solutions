487c81ae63ca4bae749e1b01ab4d374f31a8a417
rudra-404 <rudrakaiser840@gmail.com>
2025-06-01 12:13:53 +0600

    C++

---
+++ SOLVE/1696B - NIT Destroys the Universe.cpp
@@ -0,0 +1,41 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    ll arr[n + 1];
+    arr[0] = 0;
+    for (int i = 1; i < n + 1; i++) {
+        cin >> arr[i];
+    }
+    int op = 0;
+    for (int i = 1; i < n + 1; i++) {
+        if (arr[i - 1] == 0 && arr[i] != 0) {
+            op++;
+        }
+    }
+    cout << (op > 2 ? 2 : op) << endl;
+}
+
+int main() {
+    fast;
+    ll test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
+}
